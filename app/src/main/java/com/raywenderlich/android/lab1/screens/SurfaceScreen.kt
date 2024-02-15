0afe465a66e5d1fd631a84864f29033c6504aee6
6pendik <120449226+6pendik@users.noreply.github.com>
2024-02-16 02:56:45 +0300

    Create MyRow

--- app/src/main/java/com/raywenderlich/android/lab1/screens/SurfaceScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/SurfaceScreen.kt
@@ -1,6 +1,7 @@
 package com.raywenderlich.android.lab1.screens
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -11,7 +12,7 @@ import com.raywenderlich.android.lab1.router.Screen
 @Composable
 fun SurfaceScreen(modifier: Modifier = Modifier) {
 
-    Box(modifier = modifier.fullMaxSize()) {
+    Box(modifier = modifier.fillMaxSize()) {
         MySurface(modifier =modifier.align(Alignment.Center))
     }
     BackButtonHandler {
